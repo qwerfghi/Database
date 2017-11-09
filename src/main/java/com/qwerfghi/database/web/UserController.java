c33327eb841930e6ab2355fa16e72bd319c4879a
Pavel Lobanok <pavellobanok@gmail.com>
2017-11-09 20:00:51 +0300

    asd

---
+++ src/main/java/com/qwerfghi/database/web/UserController.java
@@ -0,0 +1,15 @@
+package com.qwerfghi.database.web;
+
+import com.qwerfghi.database.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
